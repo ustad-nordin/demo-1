fd10a71bd91a71b56b5d7dc1aa86684d92f9bbdb
bouchtaoui-loves-dhahnin <bouchtaoui.dev@gmail.com>
2022-03-09 11:19:59 +0100

    Init...

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// Maak een array van auto merken
+const merken = ["Mercedes", "Volkswagen", "Audi", "Toyota"];
+
+// Declareer positie met initiele waarde 0
+let position = 0;
+
+// 
+const button = document.getElementsByClassName("testbutton")[0];
+button.addEventListener("click", onFetch);
+let output = document.getElementsByClassName("title")[0];
+
+function onClick() {
+    if(position == merken.length)
+        position = 0;
+    output.innerHTML = merken[position];
+    position++;
+}
+
+function onFetch() {
+    // fetch data from api
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(response => response.json())
+    .then(json => output.innerHTML = JSON.stringify(json))
+}
